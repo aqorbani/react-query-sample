4b20d7655d2c8662cf1cbe4c0de4b2ccad8df95e
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-01-26 15:05:38 +0330

    testing useQuery with axios and handle error

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,27 @@
 import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 const Home = () => {
   const getUsers = async () => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/users");
-    return res.json();
+    const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+    return res;
   };
-  const { data, isLoading } = useQuery({
+
+  const { data, isLoading, isError, error } = useQuery({
     queryKey: ["users"],
     queryFn: getUsers,
   });
 
-  console.log({ data, isLoading });
-  return <div>Home</div>;
+  if (isLoading) return <h4>Loading ...</h4>;
+  if (isError) return <h4>error:{error.message}</h4>;
+
+  return (
+    <div>
+      {data.data.map((item) => (
+        <p key={item.id}>{item.name}</p>
+      ))}
+    </div>
+  );
 };
 
 export default Home;

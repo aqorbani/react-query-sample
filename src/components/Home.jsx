58d9014dc8355013b342db8913879178bfd19c12
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-01-26 14:51:03 +0330

    testing useQuery v5 with fetch

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+
 const Home = () => {
+  const getUsers = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    return res.json();
+  };
+  const { data, isLoading } = useQuery({
+    queryKey: ["users"],
+    queryFn: getUsers,
+  });
+
+  console.log({ data, isLoading });
   return <div>Home</div>;
 };
 
